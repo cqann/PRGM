43ec5af243ea3d0348bf1fa99ac8c55549267a8e
cqann <cqann.lindberg@gmail.com>
2020-01-03 09:07:05 +0100

    Euler 36

--- Python/pgrm1/app.py
+++ Python/pgrm1/app.py
@@ -1,82 +1,42 @@
+import math
+
+def base_converter(x):
+    highest_exp = 0
+    
+
+    while 2**highest_exp <= x:
+        highest_exp += 1
+
+    res = ""
+
+    while highest_exp>= 0:
+        if (x - pow(2,highest_exp) >= 0):
+            res += "1"
+            x -= pow(2,highest_exp)
+        else:
+            res += "0"
+        
+        highest_exp -= 1
+    
+
+    return int(res[1:])
 
 
-combos = set()
-fc = (0,0,0,0,0,0,0,0)
+
+def is_pal(n):
+    nlist = list(str(n))
+    for i in range(math.ceil(len(nlist))):
+        if nlist[i] != nlist[len(nlist)-1-i]:
+            return False
+    return True
+
+
 result = 0
-
-def create(carry,c):
-    if carry >= 200:
-        if carry == 200:
-            if c in combos:
-                combos.insert(c)
-                result += 1
-        return
-    else:
-        c = list(c)
-
-        if carry + 1 <= 200:
-            c[0] += 1
-            create(carry+1,tuple(c))
-            c[0] -= 1
-        if carry + 2 <= 200:
-            c[1] += 1
-            create(carry+2,tuple(c))
-            c[1] -= 1
-        if carry + 5 <= 200:
-            c[2] += 1
-            create(carry+5,tuple(c))
-            c[2] -= 1
-        if carry + 10 <= 200:
-            c[3] += 1
-            create(carry+10,tuple(c))
-            c[3] -= 1
-        if carry + 20 <= 200:
-            c[4] += 1
-            create(carry+20,tuple(c))
-            c[4] -= 1
-        if carry + 50 <= 200:
-            c[5] += 1
-            create(carry+50,tuple(c))
-            c[5] -= 1
-        if carry + 100 <= 200:
-            c[6] += 1
-            create(carry +100,tuple(c))
-            c[6] -= 1
-        if carry + 200 <= 200:
-            c[7] += 1
-            create(carry+200,tuple(c))
-            c[7] -= 1
-        return
-
-
-
-nextCoin = {1:2, 2:5, 5:10, 10:20,20:50,50:100,100:200};
-
-def genNums(carry, curCoin):
-    global result
-    if carry == 200:
-        result += 1
-    elif carry < 200:
-        
-        limit = int(200/(curCoin)) + 1
-        for i in range(limit):
-            try:
-                genNums(carry + curCoin*i, nextCoin[curCoin])
-            except KeyError:
-                pass
-    return 
-        
-
-     
-
-#73674
-genNums(0,1)
+for i in range(1,1000000):
+    if is_pal(i) and is_pal(base_converter(i)):
+        result += i
+        print(i)
 
 print(result)
-
-
-
-
-
 
 
