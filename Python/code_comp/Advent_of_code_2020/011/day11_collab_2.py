9b66c31f1894811a02861327e6fb64f7071ead6c
EriKWDev <ErikWDev@gmail.com>
2020-12-11 17:03:43 +0100

    11_2

--- Python/code_comp/Advent_of_code_2020/011/day11_collab_2.py
+++ Python/code_comp/Advent_of_code_2020/011/day11_collab_2.py
@@ -1,6 +1,6 @@
 seat_map = []
 
-with open("day11_e.txt") as seating_file:
+with open("day11_c.txt") as seating_file:
     seat_map = [list(line.strip()) for line in seating_file]
 
 
@@ -9,28 +9,36 @@ row_length = len(seat_map[0])
 OCCUPIED = "#"
 EMPTY = "L"
 FLOOR = "."
+generation = 0
 
 def number_of_occupied_around(x, y, seat_map):
     number_of_occupied = 0
 
     for dx in range(-1, 2):
-        if x + dx < 0:
-            continue
         for dy in range(-1, 2):
-            if y+dy < 0:
-                continue
-
             if dy == 0 and dx == 0:
                 continue
 
-            try:
-                if seat_map[y+dy][x+dx] == OCCUPIED:
-                    number_of_occupied += 1
-            except IndexError:
-                continue
+            n = 1
+            stop_loop = False
+            while not stop_loop:
+                new_y = y + dy * n
+                new_x = x + dx * n
+
+                if new_y < 0 or new_x < 0:
+                    break
+
+                try:
+                    if seat_map[new_y][new_x] == OCCUPIED:
+                        number_of_occupied += 1
+                        stop_loop = True
+                    elif seat_map[new_y][new_x] == EMPTY:
+                        stop_loop = True
+                except IndexError:
+                    stop_loop = True
+                n += 1
 
     return number_of_occupied
-
 
 while True:
     change_counter = 0
@@ -39,7 +47,7 @@ while True:
     for y, row in enumerate(seat_map):
         for x, seat in enumerate(row):
             adjacent_occupied = number_of_occupied_around(x, y, seat_map)
-            if seat == OCCUPIED and adjacent_occupied >= 4:
+            if seat == OCCUPIED and adjacent_occupied >= 5:
                 new_map[y][x] = EMPTY
                 change_counter += 1
             elif seat == EMPTY and adjacent_occupied == 0:
@@ -47,6 +55,7 @@ while True:
                 change_counter += 1
 
     seat_map = new_map
+    generation += 1
 
     if change_counter == 0:
         break
