7feeaa80aaf9d37c9a7706c7b31d0ea603fc66db
EriKWDev <ErikWDev@gmail.com>
2020-12-11 15:54:22 +0100

    10_2

--- Python/code_comp/Advent_of_code_2020/010/day10_2_collab.py
+++ Python/code_comp/Advent_of_code_2020/010/day10_2_collab.py
@@ -1,17 +1,35 @@
 
-ratings = []
+ratings = [0]
 
-with open("day10_example.txt") as joltage_file:
-    ratings = sorted([int(line) for line in joltage_file])
+with open("day10_e.txt") as joltage_file:
+    ratings += sorted([int(line) for line in joltage_file])
     ratings.append(ratings[-1] + 3)
 
-diffs = {1: 0, 2: 0, 3: 0}
+ratings_dict = {rating: i for i, rating in enumerate(ratings)}
+distinct_count = 1
 
-current_rating = 0
-for rating in ratings:
-    diff = rating - current_rating
-    diffs[diff] += 1
-    current_rating = rating
+count_array = [-1] * len(ratings)
+count_array[-1] = 1
 
+def paths_under(index):
+    global ratings
 
-print(diffs[1] * diffs[3])
+    if count_array[index] != -1:
+        return count_array[index]
+
+    current_rating = ratings[index]
+
+    potential_under = [current_rating + x for x in range(1, 4)]
+    indicies_under = []
+    for n in potential_under:
+        if n in ratings_dict:
+            indicies_under.append(ratings_dict[n])
+
+    counter = 0
+    for i in indicies_under:
+        counter += paths_under(i)
+
+    count_array[index] = counter
+    return counter
+
+print(paths_under(0))
